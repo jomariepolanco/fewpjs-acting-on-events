07b4142e38f4837b289f2c31a0b33f4634852963
jomariepolanco <jomariepolanco@gmail.com>
2020-11-17 17:39:50 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+const dodger = document.querySelector("#dodger")
+// console.log(dodger)
+
+function moveDodgerLeft () {
+    let leftNumbers = dodger.style.left.replace('px', '')
+    let left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+
+function moveDodgerRight () {
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+})
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
